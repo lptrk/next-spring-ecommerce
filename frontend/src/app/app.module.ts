faa27235aafbe66b77a723b4696c9d23ee4699e4
Patrik <lktpriv@gmail.com>
2023-06-12 14:29:51 +0200

    added input-component

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -4,11 +4,13 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { SharedButtonComponent } from './shared/components/shared-button/shared-button.component';
+import { SharedInputComponent } from './shared/components/shared-input/shared-input.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    SharedButtonComponent
+    SharedButtonComponent,
+    SharedInputComponent
   ],
   imports: [
     BrowserModule,
