a6eef00b883511ce2f1c99c688c18761fb07884e
lptrk <lktpriv@gmail.com>
2023-08-12 17:13:30 +0200

    added database connection

--- src/main/java/de/lptrk/ecommerce/backend/exception/RestResponseEntityExceptionHandler.java
+++ src/main/java/de/lptrk/ecommerce/backend/exception/RestResponseEntityExceptionHandler.java
@@ -8,9 +8,8 @@ import org.springframework.web.bind.annotation.RestControllerAdvice;
 public class RestResponseEntityExceptionHandler {
 
     @ExceptionHandler(value = {EntityNotFoundException.class})
-    public ResponseEntity<?> handleNotFound() {
+    public ResponseEntity<Object> handleNotFound() {
         return ResponseEntity.notFound().build();
     }
-
 
 }
